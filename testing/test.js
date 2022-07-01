22e19763d0268b2141f79725c3793ed7444ee48b
Velislav Tsenov <velislav.tsenov@gmail.com>
2022-07-01 18:02:15 +0300

    Add: Testing directory

---
+++ testing/test.js
@@ -0,0 +1,53 @@
+const API_KEY = 'api_key=eKe4TcSvYOBAjxJYbt0cc2UESWgsz8Yq'
+const REQUEST_URL = 'https://api.giphy.com/v1/gifs/trending?'
+const LIMIT = '&limit=25'
+const RATING = '&rating=g'
+// https://api.giphy.com/v1/gifs/trending?api_key=eKe4TcSvYOBAjxJYbt0cc2UESWgsz8Yq&limit=25&rating=g
+const grid = document.querySelector('.grid');
+
+const getData = async () => {
+    const response = await fetch(REQUEST_URL+API_KEY+LIMIT+RATING)
+    const data = await response.json();
+    loadImages(data.data);
+}
+const loadImages = async (collection) => {
+    const path = collection.forEach(image => {
+        console.log(image.images.downsized.url);
+        addImageToGrid(image.images.downsized.url);
+    });
+};
+
+const addImageToGrid = (path) => {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    const img = document.createElement('img');
+    img.src = `${path}`
+    div.appendChild(img);
+    grid.appendChild(div)
+    setMasonry();
+}
+
+const setMasonry = () => {
+    new Masonry(grid, {
+        itemSelector: '.grid-item',
+        gutter: 10,
+        originLeft: false,
+        originRight: false,
+    });
+};
+
+window.addEventListener('scroll', () => {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+        getData();
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    getData();
+});
