e2ed1021f683302b4e7ab487ba83d9e46fe5727c
Velislav Tsenov <velislav.tsenov@gmail.com>
2022-07-03 01:05:07 +0300

    Update: implementation of update class

--- template/src/data/upload.js
+++ template/src/data/upload.js
@@ -1,27 +1,43 @@
-import { USER_NAME } from "../common/constants.js"
-import { API_KEY, UPLOAD_REQUEST_URL } from "../common/requests-params.js"
+import {
+    API_KEY,
+    UPLOAD_REQUEST_URL,
+    USERNAME
+} from "../common/requests-params.js"
 
 export const uploadGif = async (file) => {
     const objectToSend = createJSONObject(file);
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', UPLOAD_REQUEST_URL, true)
-    xhr.setRequestHeader('Content-type', 'application/json; charset = UTF-8');
-    xhr.send(objectToSend);
-
-    xhr.onload = () => {
-        if (xhr.status === 201) {
-            console.log(xhr.status)
+    const fetchURL = UPLOAD_REQUEST_URL + API_KEY + USERNAME
+    let xhr = new XMLHttpRequest()
+     
+    xhr.open('POST', fetchURL, true)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+    xhr.send(post);
+     
+    xhr.onload = function () {
+        if(xhr.status === 201) {
+            console.log("Post successfully created!") 
         }
     }
 }
 
 const createJSONObject = (file) => {
     let obj = {
-        "api_key": API_KEY,
-        "username": USER_NAME,
         "file": file,
     };
-    console.log(obj);
     return JSON.stringify(obj);
 }
+
+// const fetchURL = UPLOAD_REQUEST_URL + API_KEY + USERNAME
+// let response = await fetch(fetchURL, {
+//     method: 'POST',
+//     body: objectToSend,
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json',
+//     }
+// })
+
+// response = await response.json();
+// const data = response.json();
+// const status = data.status;
+// alert(status)
