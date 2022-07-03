1937613ab3e77756b503cd85f999e22e024987c6
Velislav Tsenov <velislav.tsenov@gmail.com>
2022-07-03 18:46:49 +0300

    Update: Minor changes to showing uploaded images

--- template/src/views/upload-view.js
+++ template/src/views/upload-view.js
@@ -2,7 +2,7 @@ import { getUploadedGifs } from "../data/upload.js"
 
 export const setUploadView = () => {
     return `
-    <div class="container col-md-6">
+    <div class="container col-md-6" id = "upload-controls">
         <div class="mb-5  text-center">
             <label for="Image" class="form-label"> Share your gif with the world! </label>
             <input class="form-control" type="file" id="formFile">
@@ -16,7 +16,7 @@ export const setUploadView = () => {
         <img id="frame" src="" class="img-fluid" />
     </div> 
     
-    <div class = 'container-fluid col-md-12'>
+    <div class = 'container-fluid col-md-12 align="center"' id = "uploaded-grid">
         ${setUploadedGifs()}
     </div> 
     `
@@ -27,13 +27,19 @@ const setUploadedGifs = () => {
     const uploadedGifsByUser = getUploadedGifs();
     let result = '';
     uploadedGifsByUser.forEach(gif => {
-        result += `\n <img src = ${gif}> </img>`
+        result += `\n <img src = ${gif} class = 'uploaded-images'> </img>`
     });
-
+    setMasonryUploaded();
     return result;
 }
 
-
-const addImagesToGrid = () => {
-    
-}
+const uploadedImages = document.getElementById('uploaded-grid');
+const setMasonryUploaded = () => {
+    new Masonry(uploadedImages, {
+        itemSelector: '.uploaded-images',
+        gutter: 10,
+        originLeft: false,
+        originRight: false,
+        fitWidth: true,
+    });
+};
