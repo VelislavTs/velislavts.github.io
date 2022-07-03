dc1855913ec25a92e7e72874264039113ae2d3c3
Velislav Tsenov <velislav.tsenov@gmail.com>
2022-07-03 18:20:05 +0300

    Start: Implementation of the Display Uploaded Gifs

--- template/src/views/upload-view.js
+++ template/src/views/upload-view.js
@@ -1,6 +1,4 @@
-import {
-    renderSearchData
-} from "../events/search-events.js"
+import { getUploadedGifs } from "../data/upload.js"
 
 export const setUploadView = () => {
     return `
@@ -16,36 +14,26 @@ export const setUploadView = () => {
             </div>
         </div>
         <img id="frame" src="" class="img-fluid" />
-    </div>  
+    </div> 
+    
+    <div class = 'container-fluid col-md-12'>
+        ${setUploadedGifs()}
+    </div> 
     `
 }
 
-{
-    /* <button onclick="clearImage()" class="btn btn-primary mt-3">Click me</button> 
-        <script>
-            function preview() {
-                frame.src = URL.createObjectURL(event.target.files[0]);
-            }
-            function clearImage() {
-                document.getElementById('formFile').value = null;
-                frame.src = "";
-            }
-        </script> */
+
+const setUploadedGifs = () => {
+    const uploadedGifsByUser = getUploadedGifs();
+    let result = '';
+    uploadedGifsByUser.forEach(gif => {
+        result += `\n <img src = ${gif}> </img>`
+    });
+
+    return result;
 }
 
-{
-    /* <div class="container col-md-6">
-            <div class="mb-5  text-center">
-                <label for="Image" class="form-label"> Share your gif with the world! </label>
-                <input class="form-control" type="file" id="formFile" onchange="preview()">
-            </div>
-            <div class = 'row'> 
-                <div class = 'col-md-12' align="center"> 
-                    <button type = 'button' class = 'btn btn-dark' id = "reset-button"> Reset </button> 
-                    <button type = 'button' class = 'btn btn-dark' id = "upload-button"> Upload </button> 
-                </div>
-            </div>
-            <img id="frame" src="" class="img-fluid" />
-        </div>  
-        ` */
+
+const addImagesToGrid = () => {
+    
 }
