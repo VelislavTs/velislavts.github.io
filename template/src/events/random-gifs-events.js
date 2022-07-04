c1f7851945dcd5cd61dc0b00006b65b73f130f0e
Gergana Georgieva <gerganag92@gmail.com>
2022-07-04 11:39:56 +0300

    Fix: Minor changes in appendToHome function

--- template/src/events/random-gifs-events.js
+++ template/src/events/random-gifs-events.js
@@ -1,20 +1,19 @@
 import { MAIN_CONTAINER } from "../common/constants.js";
 import { getRandomGifs } from "../data/random.js";
-import { loadSearchData } from "../requests/request-services.js";
 import { toHomeView } from "../views/home-view.js";
-import { toSearchView } from "../views/search-view.js";
 import { q } from "./helpers.js"
 
 export const renderHomePage2 = async () => {
     const randomGifs = await getRandomGifs();
-    
-   let mainContainer = q(MAIN_CONTAINER).innerHTML;
-   mainContainer = await toHomeView(randomGifs);
+    let mainContainer = q(MAIN_CONTAINER).innerHTML;
+    const homeView = await toHomeView(randomGifs);
+    mainContainer = homeView;
 }
 
 export const appendToHomePage2 = async () => {
     const randomGifs = await getRandomGifs();
     // tuk moje da izmislim nyakoi po-adekvaten nachin... 
     let mainContainer = q(MAIN_CONTAINER).innerHTML;
-    mainContainer = await toHomeView(randomGifs);
+    const homeView = await toHomeView(randomGifs);
+    mainContainer += homeView;
 }
